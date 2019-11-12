0eca7cb5a0663c8667b7e6c55895e55156322bac
giladgreen <green.gilad@gmail.com>
2019-11-12 15:50:01 +0000

    1

--- test-client/src/App.js
+++ test-client/src/App.js
@@ -6,33 +6,8 @@ import FacebookLogin from 'react-facebook-login';
 import { GoogleLogin } from 'react-google-login';
 import config from './config.json';
 
-//const pokerStatsGroupsUrlPrefix = URL_PREFIX;//'http://localhost:5000/api/v2';
-const pokerStatsGroupsUrlPrefix = 'https://poker-stats.herokuapp.com/api/v2';
-let mediaStream, context, myWidth,myHeight;
-async function setupVideo(){
-    if (context) return;
-    try {
-        mediaStream = await navigator.mediaDevices.getUserMedia({audio:false,  video: true  });
-        const video = document.querySelector('video');
-        video.srcObject = mediaStream;
-        video.onloadedmetadata = function(event,error) {
-            if (error){
-                console.error('error',error);
-                return;
-            }
-            video.play();
-            const canvas = document.querySelector('canvas');
-            context = canvas.getContext('2d');
-            const ratio = video.videoWidth/video.videoHeight;
-            myWidth = video.videoWidth-100;
-            myHeight = parseInt(myWidth/ratio,10);
-            canvas.width = myWidth;
-            canvas.height = myHeight;
-        };
-    } catch(err) {
-        console.error('stream error!', err);
-    }
-}
+const pokerStatsGroupsUrlPrefix = URL_PREFIX;//'http://localhost:5000/api/v2';
+//const pokerStatsGroupsUrlPrefix = 'https://poker-stats.herokuapp.com/api/v2';
 
 
 class App extends Component {
@@ -40,7 +15,7 @@ class App extends Component {
 
     constructor() {
         super();
-        this.state = { loading: false, isAuthenticated: false, user: null, token: null, groups:[],group:null, newGroupName:'', provider:'', error:null, videoPage:false, takingSnapshots: false, stackSize: null, interval:30000, baseChipColor: "Black", numberOfBaseChips: 1, stackImage: null };
+        this.state = { loading: false, isAuthenticated: false, user: null, token: null, groups:[],group:null, newGroupName:'', provider:'', error:null};
     }
 
     logout = () => {
@@ -49,15 +24,11 @@ class App extends Component {
 
 
     backToGroupPage = () => {
-        this.setState({error:null, videoPage:false})
+        this.setState({error:null })
     };
 
     backToMainPage = () => {
         this.setState({group:null, error:null})
-    };
-
-    goToVideoPage = () => {
-        this.setState({error:null, videoPage: true})
     };
 
     setProvider = (provider) => {
@@ -366,122 +337,6 @@ class App extends Component {
         </div>);
     };
 
-    takeOneSnapshot = async() =>{
-        const {provider, token, baseChipColor, numberOfBaseChips: NumberOfBaseChips } = this.state;
-        const numberOfBaseChips = parseInt(NumberOfBaseChips);
-        const video = document.querySelector('video');
-        context.fillRect(0,0,myWidth,myHeight);
-        context.drawImage(video,0,0,myWidth,myHeight);
-        const canvas = document.querySelector('canvas');
-        const image = canvas.toDataURL(); // PNG is the default
-        const options = {
-            url: `${pokerStatsGroupsUrlPrefix}/player-stack-image/`,
-            method:'POST',
-            headers:{
-                provider: provider,
-                "x-auth-token": token,
-                "Content-Type":'application/json'
-            },
-            body: JSON.stringify({ image, baseChipColor, numberOfBaseChips })
-        };
-
-        request(options, (error, response, body) =>{
-            if (response.statusCode>=400){
-                const bodyObj = JSON.parse(body) ;
-                console.log('error', bodyObj);
-                this.setState({error: bodyObj.title});
-
-            } else {
-                const bodyObj = JSON.parse(body) ;
-                this.setState({stackSize: bodyObj.stack, info:bodyObj.info, stackImage: bodyObj.image  });
-                console.log('server response was OK', bodyObj)
-            }
-        });
-    };
-
-    resetChipBase = ()=>{
-        const {provider, token} = this.state;
-        const options = {
-            url: `${pokerStatsGroupsUrlPrefix}/player-stack-image/`,
-            method:'DELETE',
-            headers:{
-                provider: provider,
-                "x-auth-token": token,
-                "Content-Type":'application/json'
-            }
-        };
-
-        request(options, (error, response, body) =>{
-            if (response.statusCode>=400){
-                const bodyObj = JSON.parse(body) ;
-                console.log('error', bodyObj);
-            } else {
-                const bodyObj = JSON.parse(body) ;
-                this.setState({stackSize: null, info:[]  });
-            }
-        });
-    };
-
-    startTakingSnapshots = ()=>{
-        this.takeOneSnapshot();
-        const intervalId = setInterval(this.takeOneSnapshot, this.state.interval);
-        this.setState({takingSnapshots:true, intervalId});
-    };
-
-    stopTakingSnapshots = ()=>{
-        if (this.state.intervalId){
-            clearInterval(this.state.intervalId)
-        }
-        this.setState({takingSnapshots:false, intervalId: null});
-    };
-
-    handleIntervalChange = (event) =>{
-        this.setState({error:null,interval: parseInt(event.target.value)});
-    };
-    handleNumberOfBaseChipsChange = (event) =>{
-        this.setState({error:null,numberOfBaseChips: parseInt(event.target.value)});
-    };
-
-    handleBaseChipColorChange = (event) =>{
-        this.setState({error:null,baseChipColor: event.target.value});
-    };
-
-    VideoPage = () => {
-        setTimeout(setupVideo,700);
-        const {takingSnapshots} = this.state;
-        return (
-            <div className="App videoPage">
-                <div>
-                    <button className="button" onClick={this.backToGroupPage}> back to group page</button>
-                </div>
-                <hr/>
-                <div>
-                    <button className="button" onClick={this.resetChipBase}> Reset </button>
-                </div>
-                <hr/>
-                <div>
-
-                    interval: <input type="number" id="intervalTime" value={this.state.interval} onChange={this.handleIntervalChange}/><br/>
-                    number of base chips: <input type="number" id="numberOfBaseChips" value={this.state.numberOfBaseChips} onChange={this.handleNumberOfBaseChipsChange}/><br/>
-                    base chip color: <input type="text" id="baseChipColor" value={this.state.baseChipColor} onChange={this.handleBaseChipColorChange}/>
-                </div>
-                <hr/>
-                <div>
-                    <button className="button" onClick={()=> (takingSnapshots ? this.stopTakingSnapshots() : this.startTakingSnapshots())}> {takingSnapshots ?'stop': 'start taking snapshots'}</button>
-                </div>
-                <div className="errorSection">
-                    {this.state.error}
-                </div>
-                <video id="videoOfChips"/>
-                { this.state.stackImage ? <img className="imageOfStack" src={this.state.stackImage}/> : <span/>}
-                <h1>{ this.state.stackSize || this.state.stackSize === 0 ? `stack size: ${this.state.stackSize}` : ''}</h1>
-                <div className="stackInfo">
-                    {this.state.info ? (<ul>{ this.state.info.map(item=>(<li key={item.color}>{JSON.stringify(item)}</li>)) }  </ul>) : <div/>}
-                 </div>
-                <canvas id="canvas" className="imageCanvas" width="600" height="300"/>
-
-             </div> );
-     }
 
     GroupPage = () => {
         const {group} = this.state;
@@ -489,7 +344,6 @@ class App extends Component {
             <div className="App">
                 <div>
                     <button className="button" onClick={this.backToMainPage}> back to all groups</button>
-                    <button className="button" onClick={this.goToVideoPage}> video page</button>
 
                 </div>
                 <div>
@@ -508,10 +362,7 @@ class App extends Component {
     };
 
     render() {
-        const { Header, GroupsInfo, About, LoaderPage, LoginPage, GroupPage, VideoPage } = this;
-        if (this.state.videoPage) {
-            return <VideoPage/>;
-        }
+        const { Header, GroupsInfo, About, LoaderPage, LoginPage, GroupPage } = this;
 
         return (
             <div className="App">
