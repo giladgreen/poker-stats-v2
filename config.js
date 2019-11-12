7cb6b820b9bb0c8b2727a0aac414d4a61b9bea9d
giladgreen <green.gilad@gmail.com>
2019-11-12 19:08:54 +0000

    a

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   FACEBOOK_AUTH_CLIENT_SECRET: process.env.FACEBOOK_AUTH_CLIENT_SECRET || '***',
   GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID || '819855379342-19qbfif99mdphll36hl6tbndrvquj89m.apps.googleusercontent.com',
   GOOGLE_AUTH_CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET || '***m',
-  STORAGE: process.env.STORAGE || 'DB',
+  STORAGE: process.env.STORAGE || 'LOCAL',
   DATABASE_URL: process.env.DATABASE_URL || 'postgres://rfis:12345@localhost:5432/pokerstats',
   URL_PREFIX: process.env.URL_PREFIX || 'http://localhost:5000/api/v2',
 };
