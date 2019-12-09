183691c3f4df86dfe0533ba2d8db8ceebc513a3d
giladgreen <green.gilad@gmail.com>
2019-12-09 13:56:31 +0200

    a

--- test-client/src/url.js
+++ test-client/src/url.js
@@ -1,4 +1,4 @@
-let URL_PREFIX = `${window.location.origin}api/v2`;
+let URL_PREFIX = `${window.location.origin}/api/v2`;
 if (URL_PREFIX.includes(':3000')){
     URL_PREFIX= URL_PREFIX.replace(':3000', ':5000')
 }
