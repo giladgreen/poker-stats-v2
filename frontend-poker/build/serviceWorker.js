745578e3dd29be3f722bd58f682482e2f74b6094
giladgreen <green.gilad@gmail.com>
2020-03-04 10:24:19 +0200

    a

---
+++ frontend-poker/build/serviceWorker.js
@@ -0,0 +1,31 @@
+'use strict';
+let eventLink = null;
+function getLink(){
+    return eventLink;
+}
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const { title, link, text } = data;
+    eventLink = link;
+    const options = {
+        body: text,
+        icon: 'https://www.poker-stats.com/favicon.png',
+        badge: 'https://www.poker-stats.com/favicon.png',
+    };
+
+    const notificationPromise = self.registration.showNotification(title, options);
+    event.waitUntil(notificationPromise);
+
+
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow(getLink())
+    );
+});
