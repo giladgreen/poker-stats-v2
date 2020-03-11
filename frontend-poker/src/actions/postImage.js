36af6cd455dfdf7bf6bcada118d90bf1e1742617
giladgreen <green.gilad@gmail.com>
2020-03-11 15:51:58 +0200

    x

--- frontend-poker/src/actions/postImage.js
+++ frontend-poker/src/actions/postImage.js
@@ -5,7 +5,7 @@ import URL_PREFIX from '../url';
 async function postImage(base64image, tags, provider, token) {
 
   try {
-    base64image = resizebase64(base64image,500, 500);
+    base64image = resizebase64(base64image,430, 400);
   } catch (e) {
     console.log('error!!',e)
   }
