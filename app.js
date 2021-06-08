d773854b6bc1585efcbcd92665ec0aa369bafa1b
gilad green <gilad.green@autodesk.com>
2021-06-08 09:17:34 +0300

    a

--- app.js
+++ app.js
@@ -31,10 +31,13 @@ const PUBLIC = path.join(__dirname, 'public');
 const faviconPath = path.join(PUBLIC, 'favicon.png');
 app.use((request, response, next) => {
   if (request.method.toUpperCase() === 'OPTIONS') {
-    response.send({});
-  } else {
-    next();
+    logger.info('OPTIONS..');
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', '*');
+    response.header('Access-Control-Expose-Headers', '*');
+    return response.send({});
   }
+  return next();
 });
 app.use(compression());
 app.use(express.static(PUBLIC));
